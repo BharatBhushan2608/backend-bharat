be80c36fb3b8199350e1b706b885b14ab37e3454
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2026-02-09 12:29:22 +0530

    conneting database

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n Connected to MongoDB database !! DB HOST:${connectionInstance.connection.host} \n`);
+        
+    } catch (error) {
+        console.log("MONGODB connection ERROR", error);
+        process.exit(1);
+        
+    }
+}
+
+export default connectDB; 
