f8355318bd71b5901db0e61e3298ff7b52feed27
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2026-02-14 00:01:16 +0530

    add video and user models

---
+++ src/models/video.models.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile : {
+            type: String, // cloudinary url
+            required : true,
+        },
+
+        thumbnail : {
+            type: String, // cloudinary url
+            required : true,
+        },
+
+        tile:{
+            type : String,
+            required : true,
+        },
+        description : {
+            type : String,  
+            required : true,
+        },
+        duration:{
+            type :Number , 
+             required : true,
+        },
+
+        views:{
+            type : Number,
+            default : 0,
+        },
+        isPublished:{
+            type : Boolean,
+            default : true,
+        },
+        owner :{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            required : true,
+        }
+    },
+    {timestamps : true}
+)
+
+//  aggregation pipeline for pagination
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video" , videoSchema);
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,4 +1,4 @@
-class ApiErroe extends Error{
+class ApiError extends Error{
     constructor(
           statusCode,
           message = "Something went wrong",
